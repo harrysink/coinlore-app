7002f7d27fa0711d62f575632851863ace9c4a77
Harrison <harrisonfemi@yahoo.com>
2024-11-04 22:34:44 +0100

    my first app commit

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import { useReactTable, createColumnHelper, flexRender, getCoreRowModel, getPaginationRowModel } from "@tanstack/react-table";
+import { defaultData } from "../data/data";
+
+const Table = () => {
+    const [data, setData] = useState<defaultData[]>([]);
+    const [page, setPage] = useState(0);
+    
+    useEffect(() => {
+        const apiUrl = 'https://api.coinlore.net/api/tickers/'
+        fetch(apiUrl)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response was not okay')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data.data)
+                console.log('Data: ', data);
+            })
+            .catch(error => {
+                console.error('Error: ', error)
+            });
+    }, [])
+
+    const columnHelper = createColumnHelper<defaultData>();
+    const columns = [
+        columnHelper.accessor('name', {
+            cell: (info) => info.getValue(),
+            header: () => <span>💰Coin</span>
+        }),
+        columnHelper.accessor('symbol', {
+            cell: (info) => info.getValue(),
+            header: () => <span>📄Code</span>
+        }),
+        columnHelper.accessor('price_usd', {
+            cell: (info) => info.getValue(),
+            header: () => <span>🤑Price</span>
+        }),
+        columnHelper.accessor('tsupply', {
+            cell: (info) => (<>{info.getValue()} {info.row.original.symbol}</>),
+            header: () => <span>📈Total Supply</span>
+        }),
+    ]
+
+    const table = useReactTable({
+       data: data, 
+       columns, 
+       getCoreRowModel: getCoreRowModel(),
+       getPaginationRowModel: getPaginationRowModel(),
+       
+    })
+
+    return (
+        <div className="justify-items-center items-center w-[100%]">
+            <table className="border text-left shadow-lg mt-20 mb-20 w-[60%]">
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th className="text-left pt-2 pb-2 pl-2 mr-5" key={header.id}>
+                                    {header.isPlaceholder 
+                                      ? null 
+                                      : flexRender (
+                                          header.column.columnDef.header,
+                                          header.getContext(),
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id} className={`${row.index % 2 !== 0 ? "bg-[#FFFFFF]" : "bg-[#E4E4E4]"}`}>
+                            {row.getVisibleCells().map(cell => (
+                                <td className="pt-2 pb-2 pl-4 pr-4" key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+                {/* pagination buttons */}
+                <div className="flex justify-between">
+                    <button
+                        className="font-bold p-3 pt-2 pb-2 pl-4 pr-4 top-0" 
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                        hidden={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </button>
+                    <button 
+                        className="font-bold p-3 pt-2 pb-2 pl-4 pr-4 top-0" 
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                        hidden={!table.getCanNextPage()}
+                    >
+                        Next 
+                    </button>
+                </div>
+            </table>
+        </div>
+    );
+}
+
+export default Table;
